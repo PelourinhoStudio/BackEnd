601ae02c53f048cb08e670e84f55e2a9412ee999
Pedro Costa <63262702+pedrocosta24@users.noreply.github.com>
2021-11-04 20:45:25 +0000

    Implemented CORS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
-import { router } from "./router/router"
+import { router } from "./router/router";
 import "./database/connection";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use(router)
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server running");
 });
-
