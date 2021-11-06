483fad3116dcc23ca76ac3115643c1d2caae997d
Pedro Costa <63262702+pedrocosta24@users.noreply.github.com>
2021-11-06 15:58:37 +0000

    Updated User schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,14 @@
-import { model, Schema } from 'mongoose'
+import { model, Schema } from "mongoose";
 
 let UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  userType: ['ADMIN', 'DESIGNER', 'DEFAULT']
-})
+  lastOnline: { type: Date },
+  state: ["Ativo", "Inativo", "Suspenso"],
+  userType: ["ADMIN", "DESIGNER", "DEFAULT"],
+});
 
-let UserModel = model('User', UserSchema)
+let UserModel = model("User", UserSchema);
 
-export default UserModel
+export default UserModel;
