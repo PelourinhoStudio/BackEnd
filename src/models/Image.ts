52315a0ea222b3904299dd896d52a8eddfc4b125
Diogo Martins <diogomartins200214@gmail.com>
2021-11-08 14:17:12 +0000

    Models updated

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -3,12 +3,14 @@ import { model, Schema } from "mongoose";
 let ImageSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  category: ["Landscape", "Wallpaper", "Nature", "Draw"],
+  category: ["Landscape", "Wallpaper", "Nature", "Drawing"],
   tags: { type: String, required: true },
   price: { type: Number, required: true },
   year: { type: Date, required: true },
   imageType: ["real", "digital"],
   imageCDN: { type: String, required: true, unique: true },
+  likes: { type: Number, required: true, default : 0 },
+  dislikes: { type: Number, required: true, default : 0 }
 });
 
 let ImageModel = model("Images", ImageSchema);
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { model, Schema } from "mongoose";
 
 let UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
+  avatar: { type: String },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   lastOnline: { type: Date },
