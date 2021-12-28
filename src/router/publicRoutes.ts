a554cf57a66257565ddfc10ee8e5755305ee75c6
ruicosta1933 <ruicosta607@gmail.com>
2021-12-28 00:31:06 +0000

    Get image ID Public

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -6,6 +6,6 @@ const imageController = new ImagesController();
 
 router.get("/images", imageController.getAllImages);
 router.get("/categories", imageController.getCategories);
-
+router.get("/image/:id", imageController.getImageById);
 
 export default router;
