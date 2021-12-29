3851ea80055431632d0b05b42acc70c1c92212db
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-29 18:00:06 +0000

    Fixed bugs

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -1,26 +1,24 @@
 import { model, Schema } from "mongoose";
-import {UserSchema} from "@models/User";
+import { UserSchema } from "@models/User";
 
 let ImageSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   category: {
     type: [String],
-    enum: ["Landscape", "Wallpaper", "Nature", "Drawing"],
+    enum: ["landscape", "wallpaper", "nature", "drawing"],
     lowercase: true,
   },
-  tags: { type: String, required: true },
+  tags: { type: [String], required: true },
   price: { type: Number, required: true },
   year: { type: Date, required: true },
   imageType: { type: String, enum: ["real", "digital"] },
   imageCDN: { type: String, required: true, unique: true },
-  likes: { type: Number, required: true, default: 0 },
-  dislikes: { type: Number, required: true, default: 0 },
-  author: { type: Schema.Types.ObjectId, ref:"User" },
+  likes: { type: Number, default: 0 },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
-
-let User = model("User", UserSchema)
+let User = model("User", UserSchema);
 let ImageModel = model("Images", ImageSchema);
 
 export default ImageModel;
