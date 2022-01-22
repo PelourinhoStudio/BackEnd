782f5615a0e56183b3d33212f9ec46401dc1295e
unknown <ruicosta607@gmail.com>
2022-01-22 15:21:09 +0000

    User Routes Public

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import ImagesController from "@controllers/ImageController";
+import UsersController from "@controllers/UsersController";
 
 const router = express.Router();
 const imageController = new ImagesController();
+const usersController = new UsersController();
 
 router.get("/images", imageController.getAllImages);
 router.get("/categories", imageController.getCategories);
 router.get("/category/:category", imageController.getImagesByCategory);
 router.get("/image/:id", imageController.getImageById);
 
+router
+  .route("/users/:id")
+  .get(usersController.getUserById)
+  .put(usersController.update)
+  .delete(usersController.delete);
+
+  
 export default router;
